db22dabfae27f7c2fd41f8ee42d65637fa708a2d
igorkhorev <igorhorev@gmail.com>
2018-07-26 10:43:40 +0300

    remove db folder

--- db/mysql/РїСЂРѕРІРµСЂРєР° СЃС‚Р°С‚СѓСЃРѕРІ Р·Р°РґР°С‡.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-id,task_progress_status(start, end, fixed, status) as x,
-DATE_FORMAT(start, '%d.%m.%y') as start ,
-DATE_FORMAT(end, '%d.%m.%y') as end ,
-DATE_FORMAT(fixed, '%d.%m.%y') as fix,
-status,
- DATE_FORMAT(CURRENT_DATE, '%d.%m.%y') as aa
-FROM v_documents 
-WHERE ( (CAST('2017-01-01' AS DATE) <= end))
